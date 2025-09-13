b245730b785694aa1428e147ceaf36ef7a3b49b1
AbhijeetKr1525 <abhijeetkumar1525@gmail.com>
2025-09-13 15:35:42 +0530

    folder added

---
+++ lab/exp5/nodetest.js
@@ -0,0 +1,15 @@
+var name="Abhijeet"
+
+var email="abhijeetkumar1525@gmail.com"
+
+console.log("Name:"+name)
+console.log("Email:"+email)
+
+var a=10
+var b=20
+
+var sum=a+b
+console.log("sum:"+sum)
+
+var diff=b-a
+console.log("Difference:"+diff)
